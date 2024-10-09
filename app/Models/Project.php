34efba591bb372626c94e7b6ee952d0bd445e989
estanho <estanhoeu@gmail.com>
2024-10-09 00:54:18 -0300

    Aula 02 - Finalizada

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\ProjectStatus;
+use App\Models\Proposal;
 
 class Project extends Model
 {
@@ -13,6 +15,16 @@ class Project extends Model
     public function casts() {
         return [
             'tech_stack' => 'array',
+            'status' => ProjectStatus::class,
+            'ends_at' => 'datetime',
         ];
+    }
+
+    public function author() {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function proposals() {
+        return $this->hasMany(Proposal::class);
     }
 }
--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -17,7 +17,7 @@ class ProjectFactory extends Factory
             'description' => fake()->randomHtml(),
             'ends_at' => fake()->dateTimeBetween('now', '+3 days'),
             'status' => fake()->randomElement(['open', 'closed']),
-            'tech_stack' => fake()->randomElement(['react', 'php', 'laravel', 'vue', 'tailwind', 'javascript', 'nextjs', 'python']),
+            'tech_stack' => fake()->randomElements(['nodejs', 'react', 'javascript', 'vite', 'nextjs'], random_int(1, 5)),
             'created_by' => User::factory(),
         ];
     }
